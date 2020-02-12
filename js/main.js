49e4dfd5d2c02d97168c76f83b2b95f8bc46339e
steschu77 <steschu77@gmail.com>
2020-02-12 17:18:53 +0100

    Add more test cases

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
+// MIT License
+
+// Copyright (c) 2019 Erin Catto
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+let demo = initDemo4();
+
 // ----------------------------------------------------------------------------
-function main()
+function initDemo1()
 {
   let world = initWorld();
+  let objs = [];
+  objs.push(world.addStaticBody({ pos: { u0: 0, u1: -10 }, rot: 0, width: { u0: 100, u1: 20 } }));
+  objs.push(world.addDynamicBody({ pos: { u0: 0, u1: 4 }, rot: 0, width: { u0: 1, u1: 1 }, mass: 200 }));
+  return { objs: objs, world: world };
+}
 
+// ----------------------------------------------------------------------------
+function initDemo2()
+{
+  let world = initWorld();
   let objs = [];
   objs.push(world.addStaticBody({ pos: { u0: 0, u1: -10 }, rot: 0, width: { u0: 100, u1: 20 } }));
   objs.push(world.addDynamicBody({ pos: { u0: 0, u1: 4 }, rot: 0, width: { u0: 1, u1: 1 }, mass: 200 }));
@@ -10,7 +43,61 @@ function main()
   objs.push(world.addDynamicBody({ pos: { u0: -2, u1: 8 }, rot: 0, width: { u0: 0.5, u1: 0.5 }, mass: 50 }));
   objs.push(world.addDynamicBody({ pos: { u0: -3, u1: 8 }, rot: 0, width: { u0: 0.5, u1: 0.5 }, mass: 50 }));
   objs.push(world.addDynamicBody({ pos: { u0: 1, u1: 12 }, rot: 0, width: { u0: 1, u1: 1 }, mass: 200 }));
+  return { objs: objs, world: world };
+}
 
+// ----------------------------------------------------------------------------
+function initDemo3()
+{
+  let world = initWorld();
+  let objs = [];
+  objs.push(world.addStaticBody({ pos: { u0:  0.0,  u1: -14 }, rot:  0.0,  width: { u0: 100, u1: 20 } }));
+  objs.push(world.addStaticBody({ pos: { u0: -2.0,  u1: 7.0 }, rot: -0.25, width: { u0: 13, u1: 0.25 } }));
+  objs.push(world.addStaticBody({ pos: { u0:  5.25, u1: 5.5 }, rot:  0.0,  width: { u0: 0.25, u1: 1 } }));
+  objs.push(world.addStaticBody({ pos: { u0:  2.0,  u1: 3.0 }, rot:  0.25, width: { u0: 13, u1: 0.25 } }));
+  objs.push(world.addStaticBody({ pos: { u0: -5.25, u1: 1.5 }, rot:  0.0,  width: { u0: 0.25, u1: 1 } }));
+  objs.push(world.addStaticBody({ pos: { u0: -2.0, u1: -1.0 }, rot: -0.25, width: { u0: 13, u1: 0.25 } }));
+
+  for (let i = 0; i < 5; i++) {
+    objs.push(world.addDynamicBody({ pos: { u0: -7.5 + 2.0 * i, u1: 10 }, rot: 0, width: { u0: 0.5, u1: 0.5 }, mass: 25 }));
+  }
+  return { objs: objs, world: world };
+}
+
+// ----------------------------------------------------------------------------
+function initDemo4()
+{
+  let world = initWorld();
+  let objs = [];
+  objs.push(world.addStaticBody({ pos: { u0:  0.0,  u1: -14 }, rot:  0.0,  width: { u0: 100, u1: 20 } }));
+
+  for (let i = 0; i < 10; i++) {
+    let x = Math.random() * 0.2 - 0.1;
+    objs.push(world.addDynamicBody({ pos: { u0: x, u1: 0.51 + 1.05 * i }, rot: 0, width: { u0: 1, u1: 1 }, mass: 1 }));
+  }
+  return { objs: objs, world: world };
+}
+
+// ----------------------------------------------------------------------------
+function initDemo5()
+{
+  let world = initWorld();
+  let objs = [];
+  objs.push(world.addStaticBody({ pos: { u0:  0.0,  u1: -14 }, rot:  0.0,  width: { u0: 100, u1: 20 } }));
+
+  for (let i = 0; i < 12; i++) {
+    let x = -6.0 + i * 0.5625;
+    for (let j = i; j < 12; j++) {
+      objs.push(world.addDynamicBody({ pos: { u0: x, u1: 0.75 + 2.0 * i }, rot: 0, width: { u0: 1, u1: 1 }, mass: 10 }));
+      x += 1.125;
+    }
+  }
+  return { objs: objs, world: world };
+}
+
+// ----------------------------------------------------------------------------
+function main()
+{
   let wgl = initGl();
   let prev_t_sec = 0;
 
@@ -21,10 +108,10 @@ function main()
     prev_t_sec = t_sec;
 
     for (let i = 0; i < 4; i++) {
-      world.step(0.01);
+      demo.world.step(0.01);
     }
 
-    drawScene(wgl, objs);
+    drawScene(wgl, demo.objs);
     requestAnimationFrame(render);
   }
 
